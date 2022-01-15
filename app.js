ddb84e9b3becb2f21ed8fa555d7d4961b9c17045
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-15 14:25:05 -0500

    Testing HTMLGreeting method

--- app.js
+++ app.js
@@ -7,4 +7,13 @@ g.greet();
 g.greet().greet(true);
 
 g.greet().setLang('es').greet(true);
-g.greet().setLang('fr').greet(true);
+// g.greet().setLang('fr').greet(true).log();
+g.greet().setLang('es').greet(true).log();
+
+// Testing HTML Selector
+$('#login').click(function() {
+    var loginGrtr = G$('Juan', 'Marroquin');
+    $('#loginDiv').hide();
+
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+})
